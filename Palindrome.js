66c3eb2e94f2ba7f759404d9a58789d4d58c9649
Coffee-Please <47074173+Coffee-Please@users.noreply.github.com>
2020-11-10 11:00:17 -0800

    Add files via upload

---
+++ Palindrome.js
@@ -0,0 +1,40 @@
+//Palindrome Exercise
+
+function calculate(){
+    'use strict';
+	var input = document.getElementById("text").value;
+  var string = input.split("");
+  
+  //Reverse the string
+    var revString = [];
+  
+  for(var i = 0; i < string.length; i++){
+    revString.push(string[(string.length - 1) - i]);
+  }
+  
+  //go through the string forwards and backwards, comparing each letter
+  for(var j = 0; j < string.length; j++){  
+    //if they all match, continue
+    if(string[j] == revString[j]){
+    }
+    else{
+    document.getElementById("result").innerHTML = "It is not a palindrome.";
+      return false;
+    } 
+  }
+   
+    //otherwise it is a palindrome
+    document.getElementById("result").innerHTML = "It is a palindrome.";
+    return false; 
+}
+
+function init() {
+    'use strict';
+			
+// Function called when the form is submitted.
+    document.getElementById('theForm').onsubmit = calculate;
+
+  
+} // End of init() function.
+
+window.onload = init;
