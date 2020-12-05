20320517023ac59c819248bac795bcbda4c5decb
Coffee-Please <47074173+Coffee-Please@users.noreply.github.com>
2020-12-05 00:48:48 -0800

    Update Asmt_7.php

--- Asmt_7/scripts/Asmt_7.php
+++ Asmt_7/scripts/Asmt_7.php
@@ -1,17 +1,7 @@
 <?php
-    // Create JSON Array to store default values
-	  $json = array(
-		'result' => "none",
-		'length' => 0
-	  ); // end $json
-
-	//If the employess array is set
-	if(isset($_POST['employees'])) {
-		//Set the variables
-		$json['result'] = $_POST['employees'];// JSON array from Asmt_7.js
-		$json['length'] = count($json['result']); // Count the number of objects in the array
-		
-		// Output to the div		
-		echo "<p>Total Employees: ", $json['length'], "</p>";
+	// if this is the PHP page
+	if(isset($_GET['totalEmployees'])) {
+		// echo total number of employees
+		echo "<h1>Total Employees: ",  $_GET['totalEmployees'], "</h1>";
 	} // end if
 ?>
